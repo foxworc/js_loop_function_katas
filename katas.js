bf1e2adefe6f856add69410eb18c91eef0a93a47
Foxworthy <foxworc@amazon.com>
2020-01-28 13:46:07 -0500

    trying to turn in

--- katas.js
+++ katas.js
@@ -1,37 +1,104 @@
-(function(){
-    
-    
+(function () {
+
     function oneThroughTwenty() {
         const numbers = []
 
         // Your code goes below
 
+        for (let counter = 1; counter <= 20; counter++) {
+            numbers.push(counter)
+        }
+
         return numbers;
     }
+
+    console.log(oneThroughTwenty())
+
+
+
+
+
+
+
 
     function evensToTwenty() {
         const numbers = []
 
         // Your code goes below
 
+        for (let i = 1; i <= 20; i++) {
+            if (i % 2 === 0) {
+                numbers.push(i)
+            }
+
+        }
+
         return numbers;
     }
+
+    console.log(evensToTwenty())
+
+
+
+
+
+
+
+
+
+
+
+
 
     function oddsToTwenty() {
         const numbers = []
 
         // Your code goes below
 
+        for (let i = 1; i <= 20; i++) {
+
+            if (i % 2 !== 0) { numbers.push(i) }
+
+
+
+        }
+
         return numbers;
     }
+
+    console.log(oddsToTwenty())
+
+
+
+
+
+
+
+
 
     function multiplesOfFive() {
         const numbers = []
 
         // Your code goes below
 
-        return numbers;
+
+        for (let i = 5; i <= 100; i = i + 5) {
+
+            //
+           // if (i % 5 === 0) 
+                numbers.push(i)
+            
+
+    
+        }    return numbers
     }
+
+
+    console.log(multiplesOfFive())
+
+
+
+
 
     function squareNumbers() {
         const numbers = []
@@ -41,6 +108,11 @@
         return numbers;
     }
 
+
+
+
+
+    
     function countingBackwards() {
         const numbers = []
 
@@ -80,6 +152,6 @@
 
         return numbers;
     }
-    
-    
+
+
 })();
